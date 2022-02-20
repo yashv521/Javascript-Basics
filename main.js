4ff0a0083993a840047ab887fe444dfee6cc5175
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-20 21:04:12 +0530

    for/while loops

--- main.js
+++ main.js
@@ -18,3 +18,14 @@ const todos =[
 
 const todoJSON = JSON.stringify(todos);
 console.log(todoJSON);
+//for
+for(let i = 0;i<=10;i++){
+    console.log(`for Loop Number: ${i}`);
+}
+
+//while
+let i=0;
+while(i<10){
+    console.log(`While Loop Number: ${i}`);
+    i++;
+}
