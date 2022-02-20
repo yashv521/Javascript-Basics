1b555bac3c2c2eedb98aa4ebb2eeaf220785b0c2
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-20 20:03:15 +0530

    js to JSON

---
+++ main.js
@@ -0,0 +1,20 @@
+const todos =[
+    {
+        id: 1,
+        text: 'Take out Trash',
+        iscompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        iscompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        iscompleted: true
+    }
+];
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
