5469b97694d341db1feab4cd5a38e6966a230b90
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-20 21:54:07 +0530

    switch

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-//conditionals
-const x = 6;
-const y = 11;
+//ternary operators
+const x = 9;
 
-if(x > 5 && y>10) {
-    console.log('x is more than 5 or y is more than 10');
-};
+const color = x >10 ? 'red' : 'blue';
+
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;        
+}
