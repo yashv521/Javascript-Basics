d1f4096a1a1a4198a737ae6b58e59b87c1b79e06
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-22 12:44:00 +0530

    dom selectors

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
-//constructor function
-
-//class
-class Person{
-constructor(firstName,lastName,dob) {
-    this.firstName= firstName;
-    this.lastName=lastName;
-    this.dob= new Date(dob);
-}
-
-getBirthYear() {
-    return this.dob.getFullYear();
-}
-getFullName() {
-     return `${this.firstName} ${this.lastName}`;
-}
-
-}
-
-
-//instantiate object
-const person1 = new Person('Yash','Verma','5-02-2001');
-const person2 = new Person('Yatharth','paliwal','5-03-2001');
-
-console.log(person2.getBirthYear());
-console.log(person1);
-
-
-
+//Single element
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('h1'));
+ //multiple elements
+ console.log(document.querySelectorAll('.item'))
