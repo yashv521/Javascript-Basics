00f85225f3681eae3ab64f512026286411f2ff5e
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-22 11:05:09 +0530

    arrow

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-//ternary operators
-const x = 9;
+const addNums = num1 => num1+5;
 
-const color = x >10 ? 'red' : 'blue';
 
-switch(color) {
-    case 'red':
-        console.log('color is red');
-        break;
-    case 'blue':
-        console.log('color is blue');
-        break;
-    default:
-        console.log('color is NOT red or blue');
-        break;        
-}
+console.log(addNums(4));
+
