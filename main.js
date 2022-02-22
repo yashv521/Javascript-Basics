bb5b86f055083bfdca9f5bd74e12fe06a4a312f8
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-22 11:52:23 +0530

    oops es6

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-const addNums = num1 => num1+5;
+//constructor function
+
+//class
+class Person{
+constructor(firstName,lastName,dob) {
+    this.firstName= firstName;
+    this.lastName=lastName;
+    this.dob= new Date(dob);
+}
+
+getBirthYear() {
+    return this.dob.getFullYear();
+}
+getFullName() {
+     return `${this.firstName} ${this.lastName}`;
+}
+
+}
 
 
-console.log(addNums(4));
+//instantiate object
+const person1 = new Person('Yash','Verma','5-02-2001');
+const person2 = new Person('Yatharth','paliwal','5-03-2001');
+
+console.log(person2.getBirthYear());
+console.log(person1);
+
+
 
