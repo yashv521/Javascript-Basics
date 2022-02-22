ec3178d719fd641f10b5f1fbd34a94cff0f36034
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-22 15:12:58 +0530

    dom manipulation-2

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const ul = document.querySelector('.items');
+const btn = document.querySelector('.btn');
 
-//ul.remove();
- //ul.lastElementChild.remove();
- ul.firstElementChild.textContent = 'Hello';
- console.log(ul.children[1]);
- ul.children[1].innerText='Brad';
- ul.lastElementChild.innerHTML='<h1>Hello</h1>';
-
- const btn = document.querySelector('.btn');
- btn.style.background = 'red';
+btn.addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    document.querySelector('#my-form').style.background='#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML ='<h1>Hello</h1>';
+});
 
 
