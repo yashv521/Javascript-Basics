d5a9f6b0c70c74c9b9cdb533cb651acb9572212e
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-23 22:58:01 +0530

    dom manipulation-3

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-const btn = document.querySelector('.btn');
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-btn.addEventListener('mouseover', (e) => {
+myform.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
     e.preventDefault();
-    console.log(e.target);
-    document.querySelector('#my-form').style.background='#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML ='<h1>Hello</h1>';
-});
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} :${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
 
 
